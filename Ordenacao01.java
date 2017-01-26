bd2d67be53513be54617a0287fb9e4a0f7a5c7b6
rochaalexandree <rochaalexandree@gmail.com>
2017-01-26 13:35:40 -0300

    Update Ordenacao01.java

--- Ordenacao01.java
+++ Ordenacao01.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Ordenacao01{
@@ -13,13 +9,11 @@ public class Ordenacao01{
         int indiceMenor;
         int cont = 0;
 
-        long tempoInicial = System.currentTimeMillis();
         for (int i = 0; i < vetor.length - 1; i++) {
-            // antes de comparar, considera-se menor o valor atual do loop
+
             menor = vetor[i];
             indiceMenor = i;
 
-            // compara com os outros valores do vetor
             for (int j = i + 1; j < vetor.length; j++){
                 if (vetor[j] < menor){
                     menor = vetor[j];
@@ -27,14 +21,10 @@ public class Ordenacao01{
                 }
             }
 
-            // troca os valores menor e maior
             vetor[indiceMenor] = vetor[i];
             vetor[i] = menor;
             cont++;
         }
-        long tempoFinal = System.currentTimeMillis();
-
-        System.out.println("Trocas "+cont+" Tempo:"+(tempoFinal - tempoInicial) / 1000d);
 
         for(int i = 0; i < vetor.length; i++){
             System.out.println(vetor[i]);
@@ -69,34 +59,30 @@ public class Ordenacao01{
         }
     }
 
-    /** MergeSort - particiona o vetor ao meio
+    /** MergeSort - particiona o vetor ao meio varias vezes, transformando em subproblemas
     */
         private static int[] numbers;
         private static int[] helper;
 
         private static int number;
 
-        public static void sort(int[] values) {
-                numbers = values;
-                number = values.length;
+        public static void sort(int[] vetor) {
+                numbers = vetor;
+                number = vetor.length;
                 helper = new int[number];
                 mergesort(0, number - 1);
 
-                for(int i = 0; i < values.length; i++){
-                    System.out.println(values[i]);
+                for(int i = 0; i < vetor.length; i++){
+                    System.out.println(vetor[i]);
                 }
         }
 
         private static void mergesort(int low, int high) {
-                // check if low is smaller then high, if not then the array is sorted
+
                 if (low < high) {
-                        // Get the index of the element which is in the middle
                         int middle = low + (high - low) / 2;
-                        // Sort the left side of the array
                         mergesort(low, middle);
-                        // Sort the right side of the array
                         mergesort(middle + 1, high);
-                        // Combine them both
                         merge(low, middle, high);
                 }
 
@@ -104,7 +90,6 @@ public class Ordenacao01{
 
         private static void merge(int low, int middle, int high) {
 
-                // Copy both parts into the helper array
                 for (int i = low; i <= high; i++) {
                         helper[i] = numbers[i];
                 }
@@ -112,8 +97,7 @@ public class Ordenacao01{
                 int i = low;
                 int j = middle + 1;
                 int k = low;
-                // Copy the smallest values from either the left or the right side back
-                // to the original array
+
                 while (i <= middle && j <= high) {
                         if (helper[i] <= helper[j]) {
                                 numbers[k] = helper[i];
@@ -124,7 +108,7 @@ public class Ordenacao01{
                         }
                         k++;
                 }
-                // Copy the rest of the left side of the array into the target array
+
                 while (i <= middle) {
                         numbers[k] = helper[i];
                         k++;
@@ -136,17 +120,16 @@ public class Ordenacao01{
     /**QuickSort - Se assemelha com o pensamento do merge, mas usufrui da particição através de um pivô
     */
     private static void quickSort(int[] vetor, int inicio, int fim) {
-               for(int i = 0; i < vetor.length; i++)
-                    System.out.println(vetor[i]);
 
                if (inicio < fim) {
                       int posicaoPivo = separar(vetor, inicio, fim);
                       quickSort(vetor, inicio, posicaoPivo - 1);
                       quickSort(vetor, posicaoPivo + 1, fim);
                }
+
                for(int i = 0; i < vetor.length; i++){
                     System.out.println(vetor[i]);
-                }
+               }
          }
 
          private static int separar(int[] vetor, int inicio, int fim) {
@@ -175,56 +158,56 @@ public class Ordenacao01{
             HeapSort
         */
 
-        private static int[] a;
+        private static int[] vector;
         private static int n;
         private static int left;
         private static int right;
         private static int largest;
 
-        public static void buildheap(int []a){
-        n=a.length-1;
+        public static void buildheap(int []vector){
+        n=vector.length-1;
         for(int i=n/2;i>=0;i--){
-            maxheap(a,i);
+            maxheap(vector,i);
         }
     }
 
-    public static void maxheap(int[] a, int i){
+    public static void maxheap(int[] vector, int i){
         left=2*i;
         right=2*i+1;
-        if(left <= n && a[left] > a[i]){
+        if(left <= n && vector[left] > vector[i]){
             largest=left;
         }
         else{
             largest=i;
         }
 
-        if(right <= n && a[right] > a[largest]){
+        if(right <= n && vector[right] > vector[largest]){
             largest=right;
         }
         if(largest!=i){
             exchange(i,largest);
-            maxheap(a, largest);
+            maxheap(vector, largest);
         }
     }
 
     public static void exchange(int i, int j){
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-        }
+        int t = vector[i];
+        vector[i] = vector[j];
+        vector[j] = t;
+    }
 
-    public static void sortHeap(int []a0){
-        a = a0;
-        buildheap(a);
+    public static void sortHeap(int []vetor){
+        vector = vetor;
+        buildheap(vector);
 
         for(int i = n; i > 0; i--){
             exchange(0, i);
             n = n - 1;
-            maxheap(a, 0);
+            maxheap(vector, 0);
         }
 
-        for(int i = 0; i < a0.length; i++){
-            System.out.println(a0[i]);
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println(vetor[i]);
         }
     }
 
